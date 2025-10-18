288f033a58a938c7076579e5967cf0d6e97f38f9
odub30 <orwhite1983@gmail.com>
2025-10-17 18:40:18 -0700

    feat: configure Prisma client with Accelerate extension
    - Import and use withAccelerate from @prisma/extension-accelerate
    - Update Prisma client initialization to use extension
    - Required for Prisma Accelerate connection pooling and caching

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,18 @@
 // src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 /**
- * PrismaClient Singleton
+ * PrismaClient Singleton with Accelerate Extension
  */
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: ReturnType<typeof createPrismaClient> | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
